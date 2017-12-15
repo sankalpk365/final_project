c2c18c39daddb2d21010a4350323808569808b1f
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-15 12:08:31 -0500

    added accounts

---
+++ collections/accounts.php
@@ -0,0 +1,25 @@
+<?php
+
+class accounts extends \database\collection
+{
+
+protected static $modelName = 'account';
+
+    public static  function findUserbyUsername($login) {
+        $tableName = get_called_class();
+	$sql = 'SELECT * FROM ' .$tableName . ' WHERE email ="' . $login.'"';
+
+	$recordsSet = self::getResults($sql);
+        if (is_null($recordsSet)) {
+        echo "False";
+        return FALSE;
+        } else {
+
+		echo "True";
+        	return $recordsSet[0];
+               }
+
+	}
+}
+
+?>
