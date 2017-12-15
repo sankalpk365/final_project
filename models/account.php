1a4bb79e0ee4fd3cfc1fd245edd335df4d687df9
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-15 17:35:43 -0500

    model accounts

---
+++ models/account.php
@@ -0,0 +1,35 @@
+<?php
+final class account extends \database\model
+{
+
+    public $id;
+    public $email;
+    public $fname;
+    public $lname;
+    public $phone;
+    public $birthday;
+    public $gender;
+    public $password;
+    protected static $modelName = 'account';
+        public static function getTablename()
+	    {
+	    $tableName = 'accounts';
+	    return $tableName;
+	    }
+	public static function findTasks()
+	    {
+	    $records = todos::findAll();
+            print_r($records);
+            return $records;
+            }
+	public function setPassword($password) {
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            return $password;
+	    }
+	public function checkPassword($password) {
+            $checkpsw = password_verify($password, $this->password);
+            print_r($checkpsw);
+            return $checkpsw;
+	    }
+    }
+?>
