6e8a7ecdf86c800a79389d9a344b80ff06fb0f0c
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-15 17:03:52 -0500

    added controller

---
+++ core/http/controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace http;
+
+class controller
+{
+static public function getTemplate($template, $data = NULL)
+    {
+            $template = 'pages/' . $template . '.php';
+	    include $template;
+     }
+}
+?>
