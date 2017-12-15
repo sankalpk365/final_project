c7bae81e36c5f26d25a0bc42c068409d3e4b1c5d
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-15 16:40:48 -0500

    added collection

--- core/database/collection.php
+++ core/database/collection.php
@@ -16,3 +16,23 @@ static public function findAll()
     $sql = 'SELECT * FROM ' . $tableName;
     return self::getResults($sql);
     }
+
+protected static function getResults($sql) {
+        $db = dbConn::getConnection();
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $class = static::$modelName;
+        $statement->setFetchMode(\PDO::FETCH_CLASS, $class);
+        $recordsSet = $statement->fetchAll();
+        return $recordsSet;
+    }
+ 
+static public function findOne($id)
+    {
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
+        $recordsSet = self::getResults($sql);
+        return $recordsSet[0];
+    }
+}
+?>
