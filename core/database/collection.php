c00502b88f2d139f2d9772edc58cbf9e5a61f153
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-15 16:24:29 -0500

    update

--- core/database/collection.php
+++ core/database/collection.php
@@ -4,35 +4,15 @@ namespace database;
 abstract class collection
 {
 
-	static public function create()
-	    {
-	    $model = new static::$modelName;
-	    return $model;
-	    }
-	
-	static public function findAll()
-	    {
-	    $tableName = get_called_class();
-	    $sql = 'SELECT * FROM ' . $tableName;
-	    return self::getResults($sql);
-	    }
+static public function create()
+    {
+    $model = new static::$modelName;
+    return $model;
+    }
 
-	protected static function getResults($sql) {
-	    $db = dbConn::getConnection();
-	    $statement = $db->prepare($sql);
-	    $statement->execute();
-	    $class = static::$modelName;
-	    $statement->setFetchMode(\PDO::FETCH_CLASS, $class);
-	    $recordsSet = $statement->fetchAll();
-	    return $recordsSet;
-	    }
-
-	static public function findOne($id)
-	    {
-	    $tableName = get_called_class();
-	    $sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
-	    $recordsSet = self::getResults($sql);
-	    return $recordsSet[0];
-	    }
-}
-?>
+static public function findAll()
+    {
+    $tableName = get_called_class();
+    $sql = 'SELECT * FROM ' . $tableName;
+    return self::getResults($sql);
+    }
