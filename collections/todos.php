72aba1934763ac8fd76c4945f5425ecc58375143
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-15 12:17:00 -0500

    added todos

---
+++ collections/todos.php
@@ -0,0 +1,17 @@
+<?php
+
+class todos extends database\collection
+{
+    protected static $modelName = 'todo';
+
+    public static  function findTasksbyID($userid) {
+    $tableName = get_called_class();
+    $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid ="' . $userid.'"';
+    $recordsSet = self::getResults($sql);
+    return $recordsSet;
+    }
+
+}
+
+?>
+
