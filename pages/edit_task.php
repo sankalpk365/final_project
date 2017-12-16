7291541bd38795541abb1ac79edc7f531b45ff68
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-16 16:40:24 -0500

    updates

---
+++ pages/edit_task.php
@@ -0,0 +1,5 @@
+
+
+<form action="index.php?page=tasks&action=delete&id=<?php $data['id'] ?> " method="post" id="form1">
+    <button type="submit" form="form1" value="delete">Delete</button>
+    </form>
