5067497408a9dbacc6d3839fb03af6dcda17b669
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-16 23:41:04 -0500

    task updates

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -1,66 +1,66 @@
-<? php
+<?php
 
 class accountsController extends http\controller
 {
 
 public static function show()
     {
-    $record = accounts::findOne($_REQUEST['id']);
-    self::getTemplate('show_account', $record);
+       $record = accounts::findOne($_REQUEST['id']);
+       self::getTemplate('show_account', $record);
     }
 
 public static function all()
     {
-    $records = accounts::findAll();
-    self::getTemplate('all_accounts', $records);
+       $records = accounts::findAll();
+       self::getTemplate('all_accounts', $records);
     }
 
 public static function signup()
     {
-    self::getTemplate('new_user');
+       self::getTemplate('new_user');
     }
-
+    
 public static function register()
-    {
-    $user = accounts::findUserbyUsername($_REQUEST['email']);
-        if ($user == FALSE) {
-        $record = new account();
+{
+
+	$user = accounts::findUserbyUsername($_REQUEST['email']);
+    	if ($user == FALSE) {
+
+	$record = new account();
         $record->email = $_POST['email'];
         $record->fname = $_POST['fname'];
         $record->lname = $_POST['lname'];
         $record->phone = $_POST['phone'];
         $record->birthday = $_POST['bday'];
         $record->gender = $_POST['gender'];
-    
         $record->password = $record->setPassword($_POST['password']);
-        //print_r($record);
-	$record->save();
-	header('Location: index.php');
+        $record->save();
+        header('Location: index.php');
+}
+
+else{
+        echo 'Sorry! This email is already registered.Please try with new one!';
 	}
-	else{
-	echo 'Email is already registered !';
-	}
-    }
+}
 
 public static function store()
     {
-        print_r($_POST);
+            print_r($_POST);
     }
 
-public static function update_profile()
-    {
+public static function update_profile() {
 	$records = accounts::findOne($_REQUEST['id']);
-        $record = new account();
-        $record->id=$records->id;
-        $record->email=$_POST['email'];
-        $record->fname=$_POST['fname'];
-        $record->lname=$_POST['lname'];
-        $record->phone=$_POST['phone'];
-        $record->birthday=$_POST['birthday'];
-        $record->gender=$_POST['gender'];
-        $record->save();
-        session_start();
-        header('Location:index.php?page=accounts&action=showProf');
+   	$record = new account();
+	$record->id=$records->id;
+	$record->email=$_POST['email'];
+	$record->fname=$_POST['fname'];
+	$record->lname=$_POST['lname'];
+	$record->phone=$_POST['phone'];
+	$record->birthday=$_POST['birthday'];
+	$record->gender=$_POST['gender'];
+	$record->save();
+	session_start();
+	header('Location:index.php?page=accounts&action=showProf');
     }
 
 public static function edit_profile()
@@ -86,59 +86,51 @@ public static function editPass()
 
 public static function updatePass()
     {
-        $records = accounts::findOne($_REQUEST['id']);
-        if($records->checkPassword($_POST['currentPass']) == TRUE){
-        if($_POST['newPass1']==$_POST['newPass2']){
-        $record = new account();
-        $record->id=$records->id;
-        $record->password =$record->setPassword($_POST['newPass1']);
-	$record->save();
-	header('Location: index.php?page=accounts&action=showProf');
-        }else{
+    $records = accounts::findOne($_REQUEST['id']);
+            if($records->checkPassword($_POST['currentPass']) == TRUE){
+            if($_POST['newPass1']==$_POST['newPass2']){
+            $record = new account();
+            $record->id=$records->id;
+	    $record->password = $record->setPassword($_POST['newPass1']);
+            $record->save();
+            header('Location: index.php?page=accounts&action=showProf');
+            }else{
             echo 'Passwords do not match.';
-            }
-        }else{
-            echo 'Wrong password entered.';
-            }
-     }
+    }
+
+    }else{
+                echo 'Wrong password entered.';
+		        }
+			    }
+
 
 public static function login()
     {
+            $record = new account();
+            $record = accounts::findUserbyUsername($_POST['login']);
 
-	$record = new account();
-	$record = accounts::findUserbyUsername($_POST['login']);
-	if ($record == FALSE) {
+	    if ($record == FALSE) {
+            print_r("<h1>'Sorry! User not found,Please enter correct Login credentials....!'</h1>");
+			        } else {
+	
+            if($record->checkPassword($_POST['psw']) == TRUE) {
+	    session_start();
+	    $_SESSION["userID"] = $record->id;
+	    $_SESSION["userEmail"] = $record->email;
+	    print_r($_SESSION);
+	    header('Location:index.php?page=tasks&action=allOneUser&id='.$record->id);
+	    } else {
+	    print_r("<h1>'Sorry! You have entered wrong password....!'</h1>");
+	                }
+			        }
+				    }
 
-	print_r("<h1>'Sorry! User not found,Please enter correct Login credentials....!'</h1>");
-	        } else {
-		       if($record->checkPassword($_POST['psw']) == TRUE) {
-		session_start();
-                $_SESSION["userID"] = $record->id;
-                $_SESSION["userEmail"] = $record->email;
-                print_r($_SESSION);
-                header('Location:index.php?page=tasks&action=allOneUser&id='.$record->id);
-		} else {
-		print_r("<h1>'Sorry! You have entered wrong password....!'</h1>");
-	        }
-	 }
-    }
-
-public static function logout()
-    {
-            session_destroy();
-            header('Location: index.php');
-    }
+           public static function logout()
+	    {
+           session_destroy();
+	   header('Location: index.php');
+       }
 }
 ?>
 
-
-
-
-
-
-
-
-
-
-
-
+		    
