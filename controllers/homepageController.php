f5fd871fd4c302b1bc1a28fef7cbd0299d7c6cbd
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-17 10:32:21 -0500

    controller updates

--- controllers/homepageController.php
+++ controllers/homepageController.php
@@ -4,14 +4,12 @@ class homepageController extends http\controller
 {
     public static function show()
         {
-	$templateData['site_name'] = 'mysite';
-	self::getTemplate('homepage', $templateData);
-	}
-
-    public static function create()
-        {
-	print_r($_POST);
-        }
-}
-
-?>
+	        $templateData['site_name'] = 'WSD Project';
+		        self::getTemplate('homepage', $templateData);
+			    }
+			        public static function create()
+				    {
+				            print_r($_POST);
+					        }
+						}
+						?>
