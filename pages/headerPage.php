16b0ab3b61747215b3884891fb5da1aa8782d3d7
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-12-17 19:31:49 -0500

    updated header page

--- pages/headerPage.php
+++ pages/headerPage.php
@@ -2,13 +2,14 @@
 
 <html>
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>WSD Project - FALL2017</title>
+    <title>WSD Project </title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+    integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/fontastic.css">
     <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
@@ -18,109 +19,74 @@
     <link rel="shortcut icon" href="favicon.png">
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    </head>
+    <body>
 
-</head>
-<body>
     <div class="page home-page">
+        <!-- Main Navbar-->
     <header class="header">
-       <nav class="navbar">
-	<div class="search-box">
-               <button class="dismiss"><i class="icon-close"></i></button>
-               <form id="searchForm" action="#" role="search">
-               <input type="search" placeholder="What are you looking for..." class="form-control">
-	       </form>
-	      </div>
+    <nav class="navbar">
+               <!-- Search Box-->
+    <div class="search-box">
+    <button class="dismiss"><i class="icon-close"></i></button>
+    <form id="searchForm" action="#" role="search">
+    <input type="search" placeholder="What are you looking for..." class="form-control">
+   </form>
+  </div>
 
-	<div class="container-fluid">
-        <div class="navbar-holder d-flex align-items-center justify-content-between">
-	<div class="navbar-header">
-	<a href="https://www.njit.edu/graduatestudies/degree-programs/graduatecertificates/web-systems-development-cert/"
-	class="navbar-brand">
-        <div class="brand-text brand-big"><span>Web System Development </span><strong> ToDo Task Admin </strong></div>
-        <div class="brand-text brand-small"><strong>TD</strong></div></a>
+  <div class="container-fluid">
+                  <div class="navbar-holder d-flex align-items-center justify-content-between">
+		                      <!-- Navbar Header-->
+				                          <div class="navbar-header">
+<!-- Navbar Brand --><a
+href="https://www.njit.edu/graduatestudies/degree-programs/graduatecertificates/web-systems-development-cert/"
+class="navbar-brand">
+<div class="brand-text brand-big"><span>Web System Development </span></div>
 
-	<a id="toggle-btn" href="#" class="menu-btn active"><span></span><span></span><span></span></a>
-	                    </div>
+<ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
+                        <!-- Search-->
+<li class="nav-item d-flex align-items-center"><a id="search" href="#"><i class="icon-search"></i></a></li>
+<li class="nav-item">
+<form action="index.php?page=accounts&action=logout" method="post" id="form3" style="float:right;">
+<li><button class="btn btn-link navbar-btn" type="submit">Logout</button></li>
+          </form>
+<form action="index.php?page=accounts&action=myProf" method="post" id="form4" style="float:right;">
+ <li><span class="glyphicon glyphicon-user"></span><button class="btn btn-link navbar-btn" type="submit">My Profile</button>
+                                </li>
+                         </form>
+			                      </ul>
+            </div>
+															                  </div>
+																	          </nav>
+    </header>
 
-	<ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
-	<li class="nav-item d-flex align-items-center"><a id="search" href="#"><i class="icon-search"></i></a></li>
-	<li class="nav-item dropdown"> <a id="messages" rel="nofollow" data-target="#" href="#" data-toggle="dropdown"
-	 aria-haspopup="true" aria-expanded="false" class="nav-link"><i class="fa fa-envelope-o"></i><span class="badge bg-orange">10</span></a>
-        <ul aria-labelledby="notifications" class="dropdown-menu">
-        <li><a rel="nofollow" href="#" class="dropdown-item all-notifications text-center"> <strong>Read all messages
-   </strong></a></li>
-  </ul>
-
-  <li class="nav-item">
-  <form action="index.php?page=accounts&action=logout" method="post" id="form3" style="float:right;">
-  <li><button class="btn btn-link navbar-btn" type="submit">Logout</button></li>
-  </form>
-
-  <form action="index.php?page=accounts&action=myProf" method="post" id="form4" style="float:right;">
-  <li><span class="glyphicon glyphicon-user"></span><button class="btn btn-link navbar-btn" type="submit">My Profile</button>
-       </li>
-      </form>
-     </ul>
-    </div>
-   </div>
-  </nav>
- </header>
-</li>
-
-<div class="page-content d-flex align-items-stretch">
-
-<nav class="side-navbar">
-<div class="sidebar-header d-flex align-items-center">
-<div class="avatar"><img src="img/avatar-1.jpg" alt="..." class="img-fluid rounded-circle"></div>
-<div class="title">
-<h1 class="h4"></h1>
-<p>Web Designer</p>
-</div>
-</div>
+     <div class="page-content d-flex align-items-stretch">
+             <!-- Side Navbar -->
+	             <nav class="side-navbar">
+	<span class="heading">Main</span>
+	            <div>
+		                    <ul class="list-unstyled">
+				                        <div>
+     <li><button class="btn btn-link navbar-btn"><a href="index.php?page=tasks&action=create<?php echo$_SESSION["userID"]?>">Create Task</a></button></li>     
+     </div>
+     <nav>
 
 
-<span class="heading">Main</span>
-    <div>
-    <ul class="list-unstyled">
-    <div>
-    <li><button class="btn btn-link navbar-btn"><a href="index.php?page=tasks&action=create<?php echo $_SESSION["userID"] ?>">Create Task</a></button></li>
-    </div>
-   <li><a href="#dashvariants"	aria-expanded="false" data-toggle="collapse"><i class="icon-interface-windows"></i>WSD
-   Assignments Fall 2017 </a>
-														                        <ul id="dashvariants" class="collapse list-unstyled">
-        <li><a href="https://web.njit.edu/~hh292/Refactor_ActiveRecords/">Refactor Active</a></li>
-	<li><a href="https://web.njit.edu/~hh292/Project_new/index2.php/">Project1:CSV upload</a></li>
-	<li><a href="https://web.njit.edu/~hh292/Week04_Assignment/">PHP Basic</a></li>
-        </ul>
-       </li>
-      </nav>
      <div class="content-inner">
 
-     <button type="button" data-toggle="collapse" data-target="#style-switch" id="style-switch-button" class="btn btn-primary
-     btn-sm hidden-xs hidden-sm"><i class="fa fa-cog fa-2x"></i></button>
-             <div id="style-switch" class="collapse">
-	                 <h4 class="mb-3">Select theme colour</h4>
-			             <form class="mb-3">
-
-<select name="colour" id="colour" class="form-control">
- <option value="">select colour variant</option>
- <option value="default">violet</option>
- <option value="pink">pink</option>
- <option value="red">red</option>
- <option value="green">green</option>
- <option value="sea">sea</option>
- <option value="blue">blue</option>
- </select>
-  </form>
-   </div>
-
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script src="vendor/popper.js/umd/popper.min.js"> </script>
-        <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-        <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
-        <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-        <script src="js/charts-home.js"></script>
-        <script src="js/front.js"></script>
-	</body>
-	</html>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
+integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
+integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+     <script src="vendor/popper.js/umd/popper.min.js"> </script>
+     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+     <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+     <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+     <script src="js/charts-home.js"></script>
+     <script src="js/front.js"></script>
+     </body>
+     </html>
